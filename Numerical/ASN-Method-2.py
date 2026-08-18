9009c6287d3b9e7d8e8b7a527d5e6c711f4d021f
Chandramouli <chandramouligg6@gmail.com>
2026-08-18 09:09:46 +0530

    Fix formatting and add whitespace for improved readability in ASN calculation script

--- Numerical/ASN-Method-2.py
+++ Numerical/ASN-Method-2.py
@@ -7,6 +7,7 @@ def countDigits(n):
         n = n // 10
         count += 1
     return count  
+
 def isASN(n):
     temp = abs(n)    
     if n < 0 :
@@ -19,12 +20,15 @@ def isASN(n):
     if temp < 0 :
        asn = asn * (-1)
     return temp ==asn
+
 start = int(input("Enter the start of range: "))
 end = int(input("Enter the end range: "))
+
 print(f"The Arm Strong Number between {start} and {end}: ")
 for num in range(start,end +1):
     if isASN(num):
         print(num,end=" ")
+        
 print(f"\nThe NON Arm Strong Number between {start} and {end}:")
 for num in range(start,end +1):
     if not isASN(num):
