cccbb036d8cf260c2833f55465f3d26f815e22c9
Chandramouli <chandramouligg6@gmail.com>
2026-08-23 12:12:11 +0530

    Remove extra blank lines in REC-CountDigits.py for improved readability

--- Numerical/Recursion/REC-CountDigits.py
+++ Numerical/Recursion/REC-CountDigits.py
@@ -11,6 +11,9 @@ n = int(input("Enter the number: "))
 result = recursiveCountDigits(n,0)
 print("The count of numbers are: ",result)
 
+
+
+
 # Method-2 with code optimiziation
 
 def recursive_CountDigits(n,count):
