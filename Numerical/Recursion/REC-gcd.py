94ee7613108cf4f237172f761f47b130257dae6a
Chandramouli <chandramouligg6@gmail.com>
2026-08-24 09:23:11 +0530

    Add recursive functions for factor display and GCD calculation

---
+++ Numerical/Recursion/REC-gcd.py
@@ -0,0 +1,27 @@
+# WAP to display the GCD of two number using recursion (Eculidian Algorithm).
+
+def GCD(n1,n2):
+    if n1 == 0:
+        return n2
+    if n1 < n2:
+        n1, n2 = n2, n1
+    return GCD((n1 % n2), n2)
+
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+
+res = GCD(num1,num2)
+print(res)
+
+
+
+def recGCD(n1, n2):
+    if n2 == 0:
+        return n1
+    return GCD(n2, n1 % n2)
+
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+result = recGCD(n1, n2)
+print(result)
